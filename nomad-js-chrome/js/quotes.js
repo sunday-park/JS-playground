fa322500fbeb057e7848365468d2fecf4d067ca4
sunday-park <sundaypark.it@gmail.com>
2024-04-24 17:41:31 +0900

    chrome: quotes.js 추가

---
+++ nomad-js-chrome/js/quotes.js
@@ -0,0 +1,31 @@
+// https://api.adviceslip.com/advice
+const quote = document.querySelector("#quote span:first-child")
+
+$.ajax({
+    type: "GET",
+    url: "https://api.adviceslip.com/advice",
+    async: true,
+    dataType: 'json',
+    success: (res)=> {
+        // console.log(res.slip);
+        let advice = res.slip.advice;
+        quote.innerText = advice;
+    },
+    error: (err) => {
+        console.log(err);
+    }
+});
+
+$.ajax({
+    type: "GET",
+    url: "https://picsum.photos/v2/list?page=2&limit=100",
+    async: true,
+    success: (res)=> {
+        const chosenImage = Math.floor(Math.random() * res.length);
+        const bgUrl = res[chosenImage].download_url;
+        document.body.style.backgroundImage = `url(${bgUrl})`;
+    },
+    error: (err) => {
+        console.log(err);
+    }
+})
