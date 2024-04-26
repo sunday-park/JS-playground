3e42db4147615c76a4a2e10029047e8ffde95ee0
sunday-park <sundaypark.it@gmail.com>
2024-04-26 14:36:07 +0900

    canvas: paint_eraser 추가

--- nomad-js-canvas/js/app.js
+++ nomad-js-canvas/js/app.js
@@ -1,43 +1,83 @@
 const canvas = document.querySelector("canvas");
 const lineWidth = document.querySelector("#line-width");
+const modeBtn = document.getElementById("mode-btn");
+const resetBtn = document.getElementById("reset-btn");
+const eraserBtn = document.getElementById("eraser-btn");
+
+const color = document.getElementById("color");
+const colorOptions = Array.from(document.getElementsByClassName("color-option"));
 const ctx = canvas.getContext("2d");
 
-canvas.width = 800;
-canvas.height = 800;
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+
 ctx.lineWidth = lineWidth.value;
 let isPainting = false;
-// 목표: 마우스 드래그로 선을 그리기
+let isFilling = false;
 
-// 마우스가 움직이기 시작 할때마다 moveTo(선이 시작히는 위치)를 호출해야 함
-    // click: 마우스를 짧게 눌렀다 '떼는 것'
-    // mousedown: 마우스를 클릭한채 누르고 있는 것
-    // mouseup: (마우스를 누르고 있다가) 떼었을 때
-    // mousemove: 마우스의 움직임
-// 마우스의 클릭이 떼어질 때 lineTo(선이 끝나는 위치)가 호출되어야 함
-
-function onMove(event) {
+const onMove = (event) => {
     if (isPainting){
         ctx.lineTo(event.offsetX, event.offsetY);
-        ctx.stroke()
+        onCanvasModeClick();
         return;
     }
     ctx.moveTo(event.offsetX, event.offsetY);
 }
-function startPainting() {
+const startPainting = () => {
     isPainting = true;
 }
-function cancelPainting() {
+const cancelPainting = () => {
     isPainting = false;
-    ctx.beginPath(); // 선을 끝내는 지점에 path를 끊어준다.
+    ctx.beginPath();
 }
-
-function onlineWidthChange(event) {
+const onlineWidthChange = (event) => {
     ctx.lineWidth = event.target.value;
 }
+const onColorChange = (event) => {
+    ctx.strokeStyle = event.target.value;
+    ctx.fillStyle = event.target.value;
+}
+const onColorClick = (event) => {
+    const colorValue = event.target.dataset.color;
+    ctx.strokeStyle = colorValue;
+    ctx.fillStyle = colorValue;
+    color.value = colorValue;
+}
+const onCanvasModeClick = () => {
+    if (isFilling){
+        ctx.fillRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    } else {
+        ctx.stroke();
+    }
+}
 
-canvas.addEventListener("mousemove", onMove)
-canvas.addEventListener("mousedown", startPainting)
-canvas.addEventListener("mouseup", cancelPainting)
-canvas.addEventListener("mouseleave", cancelPainting)
+modeBtn.addEventListener("click", () => {
+    if (isFilling !== true){
+        isFilling = true;
+        modeBtn.innerText = "Fill";
+    } else {
+        isFilling = false
+        modeBtn.innerText = "Draw";
+    }
+});
+resetBtn.addEventListener("click", () => {
+    ctx.fillStyle = "white";
+    ctx.fillRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT);
+});
+eraserBtn.addEventListener("click", () => {
+    ctx.strokeStyle = "white";
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+});
 
-lineWidth.addEventListener("change", onlineWidthChange)
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click", onCanvasModeClick); //
+
+lineWidth.addEventListener("change", onlineWidthChange);
+color.addEventListener("change", onColorChange);
+colorOptions.forEach(color => color.addEventListener("click", onColorClick));
